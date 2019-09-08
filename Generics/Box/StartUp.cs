aea32b0f27ef40e103ba8d238478d8e4a24592fe
Yosifov <k.yosifov@gmail.com>
2019-09-09 00:02:42 +0300

    Add Generic Swap Method Strings Task
    For Generic Exercise
    
    closes #67

--- Generics/Box/StartUp.cs
+++ Generics/Box/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿namespace Advanced.Generics.Box
 {
     using System;
+    using System.Collections.Generic;
 
     public class StartUp
     {
@@ -8,12 +9,31 @@
         {
             int n = int.Parse(Console.ReadLine());
 
+            var listOfBoxes = new List<BoxOfString<string>>();
+
             for (int i = 0; i < n; i++)
             {
                 var currentElement = Console.ReadLine();
                 var currentBox = new BoxOfString<string>(currentElement);
-                Console.WriteLine(currentBox.ToString());
+                listOfBoxes.Add(currentBox);
             }
+
+            var indexes = Console.ReadLine()
+                .Split();
+            int firstIndex = int.Parse(indexes[0]);
+            int secondIndex = int.Parse(indexes[1]);
+
+            SwapElements(listOfBoxes, firstIndex, secondIndex);
+
+            listOfBoxes
+                .ForEach(x => Console.WriteLine(x.ToString()));
+        }
+
+        public static void SwapElements<T>(List<T> list, int firstIndex, int secondIndex)
+        {
+            var firstIndexElement = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = firstIndexElement;
         }
     }
 }
