50d7dcf73167608870d700b4621ca803de713475
Yosifov <k.yosifov@gmail.com>
2019-09-17 23:35:05 +0300

    Add ListyIterator Task
    Task for Iterators and Comparators Exercise
    
    closes #72

---
+++ IteratorsAndComparators/ListyIterator.cs
@@ -0,0 +1,58 @@
+﻿namespace Advanced.IteratorsAndComparators
+{
+    using System.Collections.Generic;
+
+    public class ListyIterator<T>
+    {
+        private List<T> elements;
+        private int currentIndex;
+
+        public ListyIterator(params T[] elements)
+        {
+            this.elements = new List<T>(elements);
+            this.currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex + 1 < this.elements.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Print()
+        {
+            if (this.elements.Count <= 0)
+            {
+                return "Invalid Operation!";
+            }
+            else
+            {
+                return $"{this.elements[this.currentIndex]}";
+            }
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+        }
+    }
+}
--- IteratorsAndComparators/StartUp.cs
+++ IteratorsAndComparators/StartUp.cs
@@ -1,30 +1,78 @@
 ﻿namespace Advanced.IteratorsAndComparators
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
 
     public class StartUp
     {
         public static void Execute()
         {
-            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
-            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
-            Book bookThree = new Book("The Documents in the Case", 1930);
+            // Lab
+            // Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            // Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            // Book bookThree = new Book("The Documents in the Case", 1930);
 
-            Library libraryOne = new Library();
+            // Library libraryOne = new Library();
 
-            SortedSet<Book> libraryTwo = new SortedSet<Book>(new BookComparator())
+            // SortedSet<Book> libraryTwo = new SortedSet<Book>(new BookComparator())
+            // {
+            //     bookOne,
+            //     bookTwo,
+            //     bookThree
+            // };
+
+            // foreach (Book book in libraryTwo)
+            // {
+            //     Console.WriteLine(book);
+            // }
+
+            // ListyIterator Exercise
+            var input = Console.ReadLine();
+
+            var commands = input.Split();
+
+            ListyIterator<string> listy;
+
+            if (commands.Length <= 1)
             {
-                bookOne,
-                bookTwo,
-                bookThree
-            };
-
-            foreach (Book book in libraryTwo)
-            {
-                Console.WriteLine(book);
+                listy = new ListyIterator<string>();
             }
+            else
+            {
+                var elements = new string[commands.Length - 1];
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    elements[i] = commands[i + 1];
+                }
+
+                listy = new ListyIterator<string>(elements);
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                switch (input)
+                {
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+                    case "Print":
+                        Console.WriteLine(listy.Print());
+                        break;
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        public static ListyIterator<T> Create<T>(params T[] elements)
+        {
+            return new ListyIterator<T>(elements);
         }
     }
 }
