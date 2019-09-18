d7043183badbf842d98eaa65f6657b7a897ff6dc
Yosifov <k.yosifov@gmail.com>
2019-09-19 00:06:02 +0300

    Add Comparing Objects
    Task for Iterators and Comparators Exercise
    
    closes #75

---
+++ IteratorsAndComparators/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace Advanced.IteratorsAndComparators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            var compareName = this.Name.CompareTo(other.Name);
+
+            if (compareName == 0)
+            {
+                var compareAge = this.Age - other.Age;
+
+                if (compareAge == 0)
+                {
+                    return this.Town.CompareTo(other.Town);
+                }
+
+                return compareAge;
+            }
+
+            return compareName;
+        }
+    }
+}
--- IteratorsAndComparators/StartUp.cs
+++ IteratorsAndComparators/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿namespace Advanced.IteratorsAndComparators
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     public class StartUp
@@ -8,19 +9,17 @@
         public static void Execute()
         {
             // Lab
+
             // Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
             // Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
             // Book bookThree = new Book("The Documents in the Case", 1930);
-
             // Library libraryOne = new Library();
-
             // SortedSet<Book> libraryTwo = new SortedSet<Book>(new BookComparator())
             // {
             //     bookOne,
             //     bookTwo,
             //     bookThree
             // };
-
             // foreach (Book book in libraryTwo)
             // {
             //     Console.WriteLine(book);
@@ -29,11 +28,8 @@
             // ListyIterator Exercise
 
             // var input = Console.ReadLine();
-
             // var commands = input.Split();
-
             // ListyIterator<string> listy;
-
             // if (commands.Length <= 1)
             // {
             //     listy = new ListyIterator<string>();
@@ -47,9 +43,7 @@
             //     }
             //     listy = new ListyIterator<string>(elements);
             // }
-
             // input = Console.ReadLine();
-
             // while (input != "END")
             // {
             //     switch (input)
@@ -73,31 +67,60 @@
             // }
 
             // Stack Exercise
-            var myStack = new MyStack<int>();
+
+            // var myStack = new MyStack<int>();
+            // var input = Console.ReadLine();
+            // while (input != "END")
+            // {
+            //     if (input.Contains("Push"))
+            //     {
+            //         var elements = input
+            //             .Substring(5, input.Length - 5)
+            //             .Split(", ")
+            //             .Select(int.Parse)
+            //             .ToArray();
+            //         myStack.Push(elements);
+            //     }
+            //     else
+            //     {
+            //         myStack.Pop();
+            //     }
+            //     input = Console.ReadLine();
+            // }
+            // PrintMyStack(myStack);
+            // PrintMyStack(myStack);
+
+            // Comparing Objects Exercise
+            var people = new List<Person>();
 
             var input = Console.ReadLine();
+
             while (input != "END")
             {
-                if (input.Contains("Push"))
-                {
-                    var elements = input
-                        .Substring(5, input.Length - 5)
-                        .Split(", ")
-                        .Select(int.Parse)
-                        .ToArray();
+                var person = input.Split();
 
-                    myStack.Push(elements);
-                }
-                else
-                {
-                    myStack.Pop();
-                }
+                var personName = person[0];
+                var personAge = int.Parse(person[1]);
+                var personTown = person[2];
+
+                people.Add(new Person(personName, personAge, personTown));
 
                 input = Console.ReadLine();
             }
 
-            PrintMyStack(myStack);
-            PrintMyStack(myStack);
+            int personIndex = int.Parse(Console.ReadLine());
+            Person personToCompare = people[personIndex - 1];
+
+            var matches = people.Where(x => x.CompareTo(personToCompare) == 0).ToList().Count;
+
+            if (matches <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matches} {people.Count - matches} {people.Count}");
+            }
         }
 
         public static ListyIterator<T> Create<T>(params T[] elements)
